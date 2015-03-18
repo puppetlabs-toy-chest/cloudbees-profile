ed394196b11956982dfb0ad36768d0694cebf671
Carl Caum <carl@carlcaum.com>
2015-03-18 14:21:24 -0700

    Manage Jenkins Enterprise demo jobs

--- manifests/jenkins_enterprise.pp
+++ manifests/jenkins_enterprise.pp
@@ -1,4 +1,12 @@
 class profile::jenkins_enterprise {
   include ::jenkins
   include ::git
+
+  jenkins::job { 'puppet-site':
+    config => template('profile/puppet-site.xml.erb'),
+  }
+
+  jenkins::job { 'profile-module':
+    config => template('profile/profile-module.xml.erb'),
+  }
 }
