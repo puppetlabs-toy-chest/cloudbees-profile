0aed2286531440269011e02a6c177cc2cf7de2a4
Carl Caum <carl@carlcaum.com>
2015-04-13 10:44:22 -0700

    Manage peadmin credentials for Jenkins and PE

--- manifests/jenkins_enterprise.pp
+++ manifests/jenkins_enterprise.pp
@@ -9,4 +9,19 @@ class profile::jenkins_enterprise {
   jenkins::job { 'profile-module':
     config => template('profile/profile-module.xml.erb'),
   }
+
+  jenkins::credentials { 'peadmin':
+    password            => '',
+    private_key_or_path => '/var/lib/jenkins/.ssh/peadmin_private_key',
+    require             => File['peadmin key'],
+  }
+
+  file { 'peadmin key':
+    content => template('profile/jenkins_pe_master_private_key'),
+    path    => '/var/lib/jenkins/.ssh/peadmin_private_key',
+    ensure  => file,
+    owner   => jenkins,
+    group   => jenkins,
+    mode    => 0600,
+  }
 }
