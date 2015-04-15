4f2fd9e6b265f6f96c7e0fb31c001857d785bb0b
Carl Caum <carl@carlcaum.com>
2015-04-15 16:25:59 -0700

    Purge all firewall by default

--- manifests/firewall.pp
+++ manifests/firewall.pp
@@ -1,17 +1,8 @@
-# Include a default set of pre and post firewall rules. This class is
-# implemented using stages in order to ensure that you will be able to declare
-# firewall rules in other classes and be assured proper order configuration of
-# the firewall without ever accidentally cutting off connectivity (even
-# temporarily).
 class profile::firewall {
   include stdlib::stages
 
   case $::osfamily {
     default: {
-
-      class { '::firewall':                stage => 'setup'  }
-      class { '::profile::firewall::pre':  stage => 'setup'  }
-      class { '::profile::firewall::post': stage => 'deploy' }
 
       resources { 'firewall':
         purge => true,
