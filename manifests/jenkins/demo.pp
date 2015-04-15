6556c21953308d5cee8cfa181971ed2b44cea063
Carl Caum <carl@carlcaum.com>
2015-04-15 16:08:19 -0700

    Remove unused key and Jenkins credential

--- manifests/jenkins/demo.pp
+++ manifests/jenkins/demo.pp
@@ -2,26 +2,13 @@ class profile::jenkins::demo {
   include ::jenkins
   include ::git
 
+  jenkins::plugin { 'swarm': }
+
   jenkins::job { 'puppet-site':
     config => template('profile/puppet-site.xml.erb'),
   }
 
   jenkins::job { 'profile-module':
     config => template('profile/profile-module.xml.erb'),
-  }
-
-  jenkins::credentials { 'peadmin':
-    password            => '',
-    private_key_or_path => '/var/lib/jenkins/.ssh/peadmin_private_key',
-    require             => File['peadmin key'],
-  }
-
-  file { 'peadmin key':
-    content => template('profile/jenkins_pe_master_private_key'),
-    path    => '/var/lib/jenkins/.ssh/peadmin_private_key',
-    ensure  => file,
-    owner   => jenkins,
-    group   => jenkins,
-    mode    => 0600,
   }
 }
