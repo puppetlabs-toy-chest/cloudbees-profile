49570f25b9d15fa6c31fb59a37bc392a30fe2d47
Carl Caum <carl@carlcaum.com>
2015-04-15 16:04:34 -0700

    Remove erroneous profiles

--- manifests/se_repo.pp
+++
@@ -1,9 +0,0 @@
-class profile::se_repo {
-  yumrepo { 'se-repo':
-    baseurl  => "http://${::servername}/rpms",
-    descr    => 'SE Cached Files',
-    enabled  => 1,
-    gpgcheck => 1,
-    gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-CentOS-6',
-  }
-}
