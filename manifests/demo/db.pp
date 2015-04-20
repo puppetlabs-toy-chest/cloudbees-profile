3ffef2ee470c2d89c4d8d5bf0a524949bc341c14
Carl Caum <carl@carlcaum.com>
2015-04-20 13:58:09 -0700

    Use hiera for parameter defaults

--- manifests/demo/db.pp
+++ manifests/demo/db.pp
@@ -1,7 +1,7 @@
 class profile::demo::db (
   $install_dir = '/opt/wordpress',
-  $db_user,
-  $db_password
+  $db_user = hiera('wordpress::db_user'),
+  $db_password = hiera('wordpress::db_password')
 ) {
   include ::mysql::server 
 }
